b5ac6f09075efe53a4e587d698d19453afed5940
Hafizh Habiibi Lubis <hafizhhabiibi123@gmail.com>
2026-06-20 22:24:05 +0700

    feat: implement fire detection UI and integrate Firebase sensor data streaming

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.uas_mobile_iot"
+    namespace = "com.miotmp.firedetection"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.uas_mobile_iot"
+        applicationId = "com.miotmp.firedetection"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
